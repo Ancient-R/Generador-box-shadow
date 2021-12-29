bc65422fb6b69ea15ed33685a181a6c83dc2e4a0
Ricardo Lima <navalimaricardo@gmail.com>
2021-12-29 08:37:06 -0600

    creacion de header

---
+++ src/components/header/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header className='header'>
+            <h1 className='header__title'>Generador de sombras</h1>
+        </header>
+    );
+}
+ 
+export default Header;
